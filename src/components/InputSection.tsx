59257157d5e0241d7b187558e39963626710d466
Shravan-github <shravansunkari07@gmail.com>
2024-06-10 22:29:10 +0530

    Initial commit some changes

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./style.css"
 
-function InputSection() {
+interface props{
+  todo:string;
+  setTodo:React.Dispatch<React.SetStateAction<string>>
+  handleAdd: (e: React.FormEvent)=> void;
+}
+
+const InputSection: React.FC<props> = ({todo, setTodo, handleAdd}) => {
+const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div>
-        <form className='input'>
-            <input type='input' placeholder='Enter a task' className='input_box'></input>
+        <form className='input' 
+        onSubmit={(e)=>{
+          handleAdd(e)
+          inputRef.current?.blur();
+        }}>
+            <input
+            ref={inputRef}
+            value={todo}
+             type='input'
+              placeholder='Enter a task'
+               className='input_box'
+               onChange={(e)=>{
+               setTodo(e.target.value)
+               }}
+               ></input>
              <button className='submit' type='submit'>Go</button>
         </form>
     </div>
   )
 }
 
-export default InputSection
+export default InputSection;
